9769b5bc6235cedf3fb328776e203bcc983f5223
Bilal Fazlani <bfazlani@seek.com.au>
2016-02-14 01:19:46 +0530

    [skip ci] added the deploy scripts back to source control

---
+++ deploy.ps1
@@ -0,0 +1,7 @@
+﻿cd artifacts\release
+if($env:APPVEYOR_REPO_NAME -eq "bilal-fazlani/Project.Json.Patcher" -And $env:APPVEYOR_REPO_BRANCH -eq "master" -And $env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Bilal Fazlani"){
+	nuget push Project.Json.Patcher.$env:APPVEYOR_BUILD_VERSION.nupkg -apikey $env:MY_NUGET_API_KEY
+}
+else{
+	"skipping deployment"
+}
