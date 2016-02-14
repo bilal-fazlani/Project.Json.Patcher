e23e9937705676e6071ad5e21a50e856312a13b4
Bilal Fazlani <bfazlani@seek.com.au>
2016-02-14 11:46:55 +0530

    build script debug

--- build.bat
+++ build.bat
@@ -13,5 +13,3 @@ cd src\Project.Json.Patcher
 call Project.Json.Patcher patch -v %APPVEYOR_BUILD_VERSION%
 
 call dnu pack --configuration release --out ..\..\artifacts
-
-cd ..\..\
--- deploy.ps1
+++ deploy.ps1
@@ -1,7 +1,24 @@
-﻿cd artifacts\release
+﻿
+cd artifacts\release
+
+"repository- $env:APPVEYOR_REPO_NAME"
+"own repo: " 
+if($env:APPVEYOR_REPO_NAME -eq 'bilal-fazlani/Project.Json.Patcher'){"true"}else{"false"}
+
+"branch- $env:APPVEYOR_REPO_BRANCH"
+"own branch: " 
+if($env:APPVEYOR_REPO_BRANCH -eq 'master'){"true"}else{"false"}
+
+"commit author- $env:APPVEYOR_REPO_COMMIT_AUTHOR" 
+"is Bilal: "
+if($env:APPVEYOR_REPO_COMMIT_AUTHOR -eq 'Bilal Fazlani'){"true"}else{"false"}
+
+
 if($env:APPVEYOR_REPO_NAME -eq "bilal-fazlani/Project.Json.Patcher" -And $env:APPVEYOR_REPO_BRANCH -eq "master" -And $env:APPVEYOR_REPO_COMMIT_AUTHOR -eq "Bilal Fazlani"){
 	nuget push Project.Json.Patcher.$env:APPVEYOR_BUILD_VERSION.nupkg -apikey $env:MY_NUGET_API_KEY
 }
 else{
 	"skipping deployment"
 }
+
+cd ../..
